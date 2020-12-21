dec03203306855c0da4c73b3f4314e92afc737b2
Anastasia-Treshchina <74378727+Anastasia-Treshchina@users.noreply.github.com>
2020-12-21 18:44:42 +0400

    Add file

---
+++ financial_planning.py
@@ -0,0 +1,13 @@
+print("Приложение для финансового планирования\n")
+
+wage = int(input("Введите заработную плату в месяц: "))
+hypothec = int(input("Введите, какой процент(%) уходит на ипотеку: "))
+spending = int(input("Введите, какой процент(%) уходит на жизнь: "))
+print("\n")
+
+wage_year = wage * 12
+hypothec_year = int(wage_year * hypothec / 100)
+spending_year = int(wage_year * spending / 100)
+print("Вывод: ")
+print("На ипотеку было потрачено: ", hypothec_year)
+print("Было накоплено: ", wage_year - hypothec_year - spending_year)
